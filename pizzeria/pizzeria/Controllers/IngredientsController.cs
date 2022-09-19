34b54f9a5db4b6d4e1915c87a09ba09fcd98c622
guill94 <86225123+guill94@users.noreply.github.com>
2022-09-19 11:28:51 +0200

    work on categories & ingredients views

--- pizzeria/pizzeria/Controllers/IngredientsController.cs
+++ pizzeria/pizzeria/Controllers/IngredientsController.cs
@@ -10,68 +10,69 @@ using pizzeria.Models;
 
 namespace pizzeria.Controllers
 {
-    public class CategoriesController : Controller
+    public class IngredientsController : Controller
     {
         private readonly AppDbContext _context;
 
-        public CategoriesController(AppDbContext context)
+        public IngredientsController(AppDbContext context)
         {
             _context = context;
         }
 
-        // GET: Categories
+        // GET: Ingredients
         public async Task<IActionResult> Index()
         {
-              return View(await _context.Categories.ToListAsync());
+              return View(await _context.Ingredients.ToListAsync());
         }
 
+       
 
-        // GET: Categories/Create
+        // GET: Ingredients/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Categories/Create
+        // POST: Ingredients/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdCategory,CategoryName")] Category category)
+        public async Task<IActionResult> Create([Bind("IdIngredient,IngredientName")] Ingredient ingredient)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(category);
+                _context.Add(ingredient);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
+            return View(ingredient);
         }
 
-        // GET: Categories/Edit/5
+        // GET: Ingredients/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Categories == null)
+            if (id == null || _context.Ingredients == null)
             {
                 return NotFound();
             }
 
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient == null)
             {
                 return NotFound();
             }
-            return View(category);
+            return View(ingredient);
         }
 
-        // POST: Categories/Edit/5
+        // POST: Ingredients/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdCategory,CategoryName")] Category category)
+        public async Task<IActionResult> Edit(int id, [Bind("IdIngredient,IngredientName")] Ingredient ingredient)
         {
-            if (id != category.IdCategory)
+            if (id != ingredient.IdIngredient)
             {
                 return NotFound();
             }
@@ -80,12 +81,12 @@ namespace pizzeria.Controllers
             {
                 try
                 {
-                    _context.Update(category);
+                    _context.Update(ingredient);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!CategoryExists(category.IdCategory))
+                    if (!IngredientExists(ingredient.IdIngredient))
                     {
                         return NotFound();
                     }
@@ -96,49 +97,49 @@ namespace pizzeria.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
+            return View(ingredient);
         }
 
-        // GET: Categories/Delete/5
+        // GET: Ingredients/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Categories == null)
+            if (id == null || _context.Ingredients == null)
             {
                 return NotFound();
             }
 
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.IdCategory == id);
-            if (category == null)
+            var ingredient = await _context.Ingredients
+                .FirstOrDefaultAsync(m => m.IdIngredient == id);
+            if (ingredient == null)
             {
                 return NotFound();
             }
 
-            return View(category);
+            return View(ingredient);
         }
 
-        // POST: Categories/Delete/5
+        // POST: Ingredients/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Categories == null)
+            if (_context.Ingredients == null)
             {
-                return Problem("Entity set 'AppDbContext.Categories'  is null.");
+                return Problem("Entity set 'AppDbContext.Ingredients'  is null.");
             }
-            var category = await _context.Categories.FindAsync(id);
-            if (category != null)
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient != null)
             {
-                _context.Categories.Remove(category);
+                _context.Ingredients.Remove(ingredient);
             }
             
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
+        private bool IngredientExists(int id)
         {
-          return _context.Categories.Any(e => e.IdCategory == id);
+          return _context.Ingredients.Any(e => e.IdIngredient == id);
         }
     }
 }
